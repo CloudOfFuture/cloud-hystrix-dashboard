afcc15cb72efa7b5cd0843766a0d036164a1b07e
Jarven <769558579@qq.com>
2017-12-21 11:56:55 +0800

    数据库配置

--- src/main/java/com/kunlun/CloudHystrixDashboardApplication.java
+++ src/main/java/com/kunlun/CloudHystrixDashboardApplication.java
@@ -2,10 +2,12 @@ package com.kunlun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableHystrixDashboard
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudHystrixDashboardApplication {
 
 	public static void main(String[] args) {
